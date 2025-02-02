1f2ec27823fbdad31e3c2e7aeb8a786e98599ef8
yuohta819 <thunhan798@gmail.com>
2025-02-02 09:41:18 +0700

    webshopacc

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
-const port = 3000
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-
+const PORT = process.env.PORT
+const URL_FRONTEND = process.env.URL_FRONTEND
 const routeClient = require("../backend/router/client/index.route")
 const routeAdmin = require("../backend/router/admin/index.route")
 const routeQTV = require("../backend/router/QTV/index.route")
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(methodOverride('_method'));
 const corsOptions = {
-  origin: 'http://localhost:5173', // Đảm bảo đây là URL frontend 
+  origin: URL_FRONTEND, // Đảm bảo đây là URL frontend 
   credentials: true,  // Cho phép gửi cookie
 };
 app.use(cors(corsOptions))
@@ -27,6 +27,6 @@ app.use(express.urlencoded({ extended: true }))
 routeClient(app)
 routeAdmin(app)
 routeQTV(app)
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT , () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
