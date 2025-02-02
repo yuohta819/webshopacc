c72e209a1708cb2d56f5264b8f9636e69aace382
yuohta819 <thunhan798@gmail.com>
2025-02-02 13:29:15 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const routeAdmin = require("../backend/router/admin/index.route")
 const routeQTV = require("../backend/router/QTV/index.route")
 const cors = require('cors')
 const methodOverride = require('method-override');
-
+app.use(cors())
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(methodOverride('_method'));
@@ -18,7 +18,7 @@ const corsOptions = {
   origin: URL_FRONTEND, // Đảm bảo đây là URL frontend 
   credentials: true,  // Cho phép gửi cookie
 };
-app.use(cors())
+
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
