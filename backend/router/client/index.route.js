c724463ab2e9509f0e08eeef761056b6a5b07eca
yuohta819 <thunhan798@gmail.com>
2025-02-02 20:00:25 +0700

    web

--- backend/router/client/index.route.js
+++ backend/router/client/index.route.js
@@ -1,4 +1,6 @@
+const Test = require("../client/test")
 const MainRoute = require("../../router/client/trangchu.route")
 module.exports = (app) => {
     app.use("https://webshopacc-12.onrender.com", MainRoute)
+    app.use("/users", Test.Test);
 }
