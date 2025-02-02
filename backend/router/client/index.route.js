4eae58d94c9b4dcdb789bb7522df058d8b875dc0
yuohta819 <thunhan798@gmail.com>
2025-02-02 21:54:37 +0700

    Update index.route.js

--- backend/router/client/index.route.js
+++ backend/router/client/index.route.js
@@ -1,6 +1,5 @@
-const Test = require("../client/test")
+
 const MainRoute = require("../../router/client/trangchu.route")
 module.exports = (app) => {
     app.use("/", MainRoute)
-    app.use("/", Test);
 }
