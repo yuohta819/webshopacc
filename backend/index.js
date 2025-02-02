ec2089b833c4fd08e2d655efade449c8bb9363a3
yuohta819 <thunhan798@gmail.com>
2025-02-02 19:07:47 +0700

    web

--- backend/index.js
+++ backend/index.js
@@ -14,16 +14,16 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(methodOverride('_method'));
 const corsOptions = {
-  origin: 'https://webshopacc.vercel.app', // Đảm bảo đây là URL frontend 
+  origin: 'https://webshopacc-13.onrender.com', // Đảm bảo đây là URL frontend 
   methods: "GET, POST, PUT, DELETE",
   allowedHeaders: "Content-Type, Authorization",
   credentials: true,  // Cho phép gửi cookie
 };
-app.get("/dangki", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.json(corsOptions))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.urlencoded({ extended: true }))
